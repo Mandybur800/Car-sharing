09566683d0512b7ce53373de580d88d8dce1e1ea
Rostyslav Mandybur <mandyburostyk@gmail.com>
2021-01-19 13:58:50 +0200

    checkstyle was added

--- src/main/java/com/manufacturing/dao/GenericDao.java
+++ src/main/java/com/manufacturing/dao/GenericDao.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 
 public interface GenericDao<T, I> {
     T create(T value);
+
     Optional<T> get(I id);
+
     List<T> getAll();
+
     T update(T value);
+
     boolean delete(I id);
 }
